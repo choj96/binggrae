cc891b335668fee33688c1cc8bc19dec9ffe142e
DG Yoon <douglasyoon9@gmail.com>
2022-11-24 16:01:29 +0900

    PC 헤더 거의 완료
    - 헤더 거의 완료

--- js/front/main.js
+++ js/front/main.js
@@ -1,22 +1,26 @@
 const header = document.querySelector(".header");
 const header_main = document.querySelector(".header-main");
-const btn_hamburger = document.querySelector(".header-hamburger");
-const hamburger_menu = document.querySelector(".hamburger-menu");
 const header_nav = document.querySelector(".header-nav");
 const header_gnb = document.querySelector(".header-gnb");
-const header_gnb_item = document.querySelectorAll(".header-gnb > li > a");
-const header_gnb_depth2 = document.querySelectorAll(".header-gnb > li > .depth2");
+const header_depth = document.querySelectorAll(".header-gnb > li > .depth2");
+const header_bg = document.querySelector(".header-bg");
+const btn_hamburger = document.querySelector(".header-hamburger");
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const btn_lang = document.querySelector(".lang-button");
+const lang_list = document.querySelector(".lang-list");
 
 // 햄버거 토글
 let hamburgerToggle = false;
 btn_hamburger.addEventListener("click", () => {
   if (hamburgerToggle === false) {
+    window.scrollTo(0, 0);
     hamburger_menu.classList.add("on");
     header_nav.classList.add("off");
     btn_hamburger.classList.add("hamburger-toggle");
     header_main.style.borderBottom = "none";
     hamburgerToggle = true;
   } else if (hamburgerToggle === true) {
+    window.scrollTo(0, 0);
     hamburger_menu.classList.remove("on");
     header_nav.classList.remove("off");
     btn_hamburger.classList.remove("hamburger-toggle");
@@ -25,25 +29,19 @@ btn_hamburger.addEventListener("click", () => {
   }
 });
 
+let langToggle = false;
+btn_lang.addEventListener("click", () => {
+  if (langToggle === false) {
+    lang_list.classList.add("lang-list-on");
+    langToggle = true;
+  } else if (langToggle === true) {
+    langToggle = false;
+    lang_list.classList.remove("lang-list-on");
+  }
+});
+
 // 스크롤 시 헤더 색상
 window.addEventListener("scroll", () => headerColor());
-
-// for (i = 0; i < header_gnb_item.length; i++) {
-//   header_gnb_item[i].addEventListener("mouseover", () => {
-//     header.style.background = "#fff";
-//     for (i = 0; i < header_gnb_item.length; i++) {
-//       header_gnb_depth2[i].style.opacity = "1";
-//       header_gnb_depth2[i].style.visibility = "visible";
-//     }
-//   });
-// }
-// header.addEventListener("mouseout", () => {
-//   for (i = 0; i < header_gnb_item.length; i++) {
-//     header_gnb_depth2[i].style.opacity = "0";
-//     header_gnb_depth2[i].style.visibility = "hidden";
-//   }
-// });
-
 function headerColor() {
   if (window.scrollY > 0) {
     header.style.background = "#fff";
@@ -51,3 +49,17 @@ function headerColor() {
     header.style.background = "transparent";
   }
 }
+header_gnb.addEventListener("mouseover", () => {
+  header_bg.classList.add("header-bg-open");
+  for (i = 0; i < header_depth.length; i++) {
+    header_depth[i].classList.add("depth2-on");
+  }
+});
+header.addEventListener("mouseout", () => {
+  header_bg.addEventListener("mouseout", () => {
+    header_bg.classList.remove("header-bg-open");
+    for (i = 0; i < header_depth.length; i++) {
+      header_depth[i].classList.remove("depth2-on");
+    }
+  });
+});
