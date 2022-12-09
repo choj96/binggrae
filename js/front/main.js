478d1bf30b363de865d6520ffc277ba6f7c7de70
h.young <cocotree221@gmail.com>
2022-12-09 17:50:44 +0900

    top 버튼 화살표 이미지 스크롤에따라 회전 적용

--- js/front/main.js
+++ js/front/main.js
@@ -45,8 +45,12 @@ window.onload = function () {
     });
 
     // 모바일 햄버거 메뉴
-    const hamburgerGnbItem = document.querySelectorAll(".hamburger-gnb > li > a");
-    const hamburgerGnbDepth = document.querySelectorAll(".hamburger-gnb > li > .depth2");
+    const hamburgerGnbItem = document.querySelectorAll(
+      ".hamburger-gnb > li > a"
+    );
+    const hamburgerGnbDepth = document.querySelectorAll(
+      ".hamburger-gnb > li > .depth2"
+    );
     let hamburgerMobileToggle = [];
     for (let i = 0; i < hamburgerGnbItem.length; i++) {
       hamburgerMobileToggle.push(false);
@@ -152,11 +156,8 @@ window.onload = function () {
     //top 버튼 기능
     const topBtn = document.getElementById("top-btn");
     topBtn.addEventListener("click", function (event) {
-      console.log(event);
-      console.log(this.tagName);
-      if (this.tagName === "A") {
-        event.preventDefault();
-      }
+      event.preventDefault();
+
       window.scrollTo({
         top: 0,
         behavior: "smooth",
@@ -164,14 +165,15 @@ window.onload = function () {
     });
 
     // 화살표 이미지 회전
-    function scrollRotate() {
-      const topBtnImg = document.getElementById("top-btn-img");
-      topBtnImg.classList.add("up");
-      // 클래스를 추가하여 스크롤시 지속회전되는것 방지
-    }
-    window.addEventListener("scroll", scrollRotate);
-
-    //top 도달시 화살표 이미지 회전 초기화
+    const topBtnImg = document.getElementById("top-btn-img");
+    window.addEventListener("scroll", function (scTop) {
+      scTop = document.documentElement.scrollTop;
+      if (scTop > 0) {
+        topBtnImg.classList.add("up");
+      } else {
+        topBtnImg.classList.remove("up");
+      }
+    });
   }
 
   // 공통 js
